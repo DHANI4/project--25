48eea367698e1635acb416f07b2db3c38b1ad655
DHANI4 <74245392+DHANI4@users.noreply.github.com>
2020-12-26 15:16:29 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,11 @@
+class Dustbin extends Baseclass {
+    constructor(x, y, width, height) {
+      super(x,y,width,height);
+      var options = {
+         isStatic:true
+      }
+      this.image=loadImage("dustbingreen.png");
+ 
+  }
+  
+};
